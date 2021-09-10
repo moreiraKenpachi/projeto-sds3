66bdd6934c2fd4fcac25f55b7a06c07e802f8834
fabricio bonete <fabriciobonete@yahoo.com.br>
2021-09-10 17:53:48 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "components/datatable";
+import Footer from "components/footer";
+import Navbar from "components/navbar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Ola tudo bem com voce...</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Ola tudo bem com voce...</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
