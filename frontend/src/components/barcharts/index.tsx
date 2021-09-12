7d7b605894cfd7a7da5e170c18738bc7de1d8d28
fabricio bonete <fabriciobonete@yahoo.com.br>
2021-09-12 02:57:49 -0400

    anotherEnv var

--- frontend/src/components/barcharts/index.tsx
+++ frontend/src/components/barcharts/index.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { type } from 'os';
+
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSuccess } from 'types/sale';
